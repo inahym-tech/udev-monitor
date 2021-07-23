fa43dc79e9c2503d74822e8ad795f6a96caf2272
inahym196 <inahym196@example.com>
2021-07-24 02:05:43 +0900

    some

--- udev_monitor/main.py
+++ udev_monitor/main.py
@@ -1,16 +1,20 @@
-import time
+import asyncio
 import pyudev
+import serial
 
 
-def main():
-    filepath = "/tmp/hello.log"
-    log = open(filepath, 'a')
-    log.write("hello!\n")
-    context = pyudev.Context()
-    log.write(str(context))
+def main(_, tty):
+    ttyPath = tty.get('DEVNAME')
+    s = serial.Serial(ttyPath)
+    while True:
+        print(ttyPath, s.read(2)[:1])
 
 
 if __name__ == '__main__':
-    while True:
-        main()
-        time.sleep(5)
+    loop = asyncio.get_event_loop()
+    future = asyncio.futures.Future()
+    context = pyudev.Context()
+    ttyList = context.list_devices(subsystem='tty', ID_VENDOR_ID='0x15ad')
+    for tty in ttyList:
+        loop.call_soon(main, future, tty)
+    res = loop.run_forever()
