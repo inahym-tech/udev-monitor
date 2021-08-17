fdb5825387c679fa940add6f5fba4b9d47bbb8c8
inahym196 <inahym196@example.com>
2021-08-17 19:07:44 +0900

    some

--- udev_monitor/main.py
+++ udev_monitor/main.py
@@ -1,77 +1,59 @@
-import re
-import asyncio
 import pyudev
-import serial_asyncio
-import random
+import serial
+import threading
+import requests
 
 
-class Serial(asyncio.Protocol):
-
-    def connection_made(self, transport):
-        self.transport = transport
-        print('port opened\n', transport)
-
-    def data_received(self, data):
-        print('data received', re.match('b\'(.)', str(data)).group(1))
-
-    def connection_lost(self, exc):
-        print('port closed (%s) \n' % exc)
-        self.transport.close()
-
-
-async def countloopTasks():
-    while True:
-        await asyncio.sleep(3)
-        print(len([task for task in asyncio.Task.all_tasks() if not task.done()]))
-
-
-async def echoHello(time):
-    print('starting ', time)
-    await asyncio.sleep(time)
-    print('ending ', time)
-    return time
-
-
-async def main(loop):
-
-    added_tasks = []
-    delays = [x for x in range(10)]
-    random.shuffle(delays)
-
-    for n in delays:
-        print('adding ', str(n))
-        task = loop.create_task(echoHello(n))
-        added_tasks.append(task)
-        await asyncio.sleep(0)
-
-    print('adding ', str(5))
-    task = loop.create_task(echoHello(5))
-    added_tasks.append(task)
-    await asyncio.sleep(5)
-
-    print('done adding tasks')
-
-    running_tasks = added_tasks[::]
-    while running_tasks:
-        running_tasks = [x for x in running_tasks if not x.done()]
-        await asyncio.sleep(0)
-
-    print('done running tasks')
-
-    results = [x.result() for x in added_tasks]
-    return results
-    #context = pyudev.Context()
-    #ttyList = context.list_devices(subsystem='tty', ID_VENDOR_ID='0x15ad')
-    # loop.create_task(countloopTasks())
-    # for tty in ttyList:
-    #    ttyPath = tty.get('DEVNAME')
-    #    coro = serial_asyncio.create_serial_connection(loop, Serial, ttyPath)
-    #    loop.create_task(coro)
-    # loop.run_forever()
+def serial_threading(tty_path, post_url):
+    ser = serial.Serial(tty_path)
+    while ser.is_open:
+        try:
+            status = ser.read(2).decode('utf-8').replace('\x00', '')
+            post_data = {'status': status}
+            try:
+                response = requests.post(post_url, json=post_data)
+            except requests.exceptions.ConnectionError:
+                print('connection error')
+            else:
+                print(response.status_code)
+        except serial.serialutil.SerialException:
+            ser.close()
+            print(tty_path, 'is close')
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    results = loop.run_until_complete(main(loop))
-    loop.close()
-    print(results)
+    context = pyudev.Context()
+    ttyList = context.list_devices(subsystem='tty', ID_VENDOR_ID='0x15ad')
+    for tty in ttyList:
+        ttyPath = tty.get('DEVNAME')
+        print('start ', ttyPath)
+        # threading.Thread(target=serial_threading(ttyPath))
+
+    monitor = pyudev.Monitor.from_netlink(context)
+    monitor.filter_by(subsystem='tty')
+    monitor.start()
+    for device in iter(monitor.poll, None):
+        action = device.action
+        if action == 'add':
+            devname = device.get('DEVNAME')
+            id_vendor_id = device.get('ID_VENDOR_ID')
+            id_serial_short = device.get('ID_SERIAL_SHORT')
+            devpath = device.get('DEVPATH')
+
+            post_url = f'http://localhost:8000/' + id_serial_short + '/'
+            post_data = {
+                'DEVNAME': devname,
+                'ID_VENDOR_ID': id_vendor_id,
+                'ID_SERIAL_SHORT': id_serial_short,
+                'DEVPATH': devpath
+            }
+            print(post_data)
+            try:
+                response = requests.post(post_url, json=post_data)
+            except requests.exceptions.ConnectionError:
+                print('connection error')
+            else:
+                print(response.status_code)
+
+            status_post_url = f'http://localhost:8000/' + id_serial_short + '/status'
+            threading.Thread(target=serial_threading(devname, status_post_url))
